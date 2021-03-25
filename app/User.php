9374aaff738d8963dfd0cd27fe781630ae4e8e87
nikoz84 <nikoz.1984@gmail.com>
2021-03-25 17:05:18 -0300

    Adicionando método em modelo User setRememberTokenAttribute para salvar quando vier preenchido

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -40,5 +41,10 @@ class User extends Authenticatable
     public function setPasswordAttribute($value)
     {
         $this->attributes['password'] =  bcrypt($value);
+    }
+
+    public function setRememberTokenAttribute($value)
+    {
+        $this->attributes['remember_token'] = $value ? Str::random(40): null;
     }
 }
